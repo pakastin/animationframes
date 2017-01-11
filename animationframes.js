b4d5c89a9aea483944da5a6dd18f0bb3f49cb174
Juha Lindstedt <juha@pakastin.fi>
2017-01-11 13:15:01 +0200

    Updates

--- animationframes.js
+++ animationframes.js
@@ -9,15 +9,44 @@
 var ticking;
 var animations = [];
 
-function frames (delay, duration) {
+var tick = function () {
   var now = Date.now();
 
-  var start = now + delay;
-  var end = start + duration;
+  if (!animations.length) {
+    ticking = false;
+    return;
+  }
+
+  for (var i = 0; i < animations.length; i++) {
+    var animation = animations[i];
+
+    if (now < animation.start) {
+      continue;
+    }
+    if (!animation.started) {
+      animation.started = true;
+      animation.startcb && animation.startcb();
+    }
+
+    var t = (now - animation.start) / (animation.end - animation.start);
+
+    animation.progresscb && animation.progresscb(t < 1 ? t : 1);
+
+    if (now > animation.end) {
+      animation.endcb && animation.endcb();
+      animations.splice(i--, 1);
+      continue;
+    }
+  }
+  requestAnimationFrame(tick);
+};
+
+var frames = function (delay, duration) {
+  var now = Date.now();
 
   var animation = {
-    start: start,
-    end: end,
+    start: now + delay,
+    end: now + delay + duration,
     started: false,
     startcb: null,
     progresscb: null,
@@ -53,83 +82,54 @@ function frames (delay, duration) {
     }
   };
   return self;
-}
-
-function tick () {
-  var now = Date.now();
-
-  if (!animations.length) {
-    ticking = false;
-    return;
-  }
-
-  for (var i = 0; i < animations.length; i++) {
-    var animation = animations[i];
-
-    if (now < animation.start) {
-      continue;
-    }
-    if (!animation.started) {
-      animation.started = true;
-      animation.startcb && animation.startcb();
-    }
-
-    var t = (now - animation.start) / (animation.end - animation.start);
-
-    animation.progresscb && animation.progresscb(t < 1 ? t : 1);
-
-    if (now > animation.end) {
-      animation.endcb && animation.endcb();
-      animations.splice(i--, 1);
-      continue;
-    }
-  }
-  requestAnimationFrame(tick);
-}
+};
 
 window.requestAnimationFrame || (window.requestAnimationFrame = function (cb) {
   setTimeout(cb, 0);
 });
 
+var easeInBy = function (power) { return function (t) { return Math.pow(t, power); }; };
+
+var easeOutBy = function (power) { return function (t) { return 1 - Math.abs(Math.pow(t - 1, power)); }; };
+
+var easeInOutBy = function (power) { return function (t) { return t < 0.5 ? easeInBy(power)(t * 2) / 2 : easeOutBy(power)(t * 2 - 1) / 2 + 0.5; }; };
+
 var ease = {
   linear: function (t) {
     return t;
   },
-  quadIn: function (t) {
-    return t * t;
-  },
-  quadOut: function (t) {
-    return t * (2 - t);
-  },
-  quadInOut: function (t) {
-    return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
-  },
-  cubicIn: function (t) {
-    return t * t * t;
-  },
-  cubicOut: function (t) {
-    return (--t) * t * t + 1;
-  },
-  cubicInOut: function (t) {
-    return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
-  },
-  quartIn: function (t) {
-    return t * t * t * t;
-  },
-  quartOut: function (t) {
-    return 1 - (--t) * t * t * t;
-  },
-  quartInOut: function (t) {
-    return t < 0.5 ? 8 * t * t * t * t : 1 - 8 * (--t) * t * t * t;
-  },
-  quintIn: function (t) {
-    return t * t * t * t * t;
-  },
-  quintOut: function (t) {
-    return 1 + (--t) * t * t * t * t;
-  },
-  quintInOut: function (t) {
-    return t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * (--t) * t * t * t * t;
+  quadIn: easeInBy(2),
+  quadOut: easeOutBy(2),
+  quadInOut: easeInOutBy(2),
+  cubicIn: easeInBy(3),
+  cubicOut: easeOutBy(3),
+  cubicInOut: easeInOutBy(3),
+  quartIn: easeInBy(4),
+  quartOut: easeOutBy(4),
+  quartInOut: easeInOutBy(4),
+  quintIn: easeInBy(5),
+  quintOut: easeOutBy(5),
+  quintInOut: easeInOutBy(5),
+  sineIn: function (t) { return 1 + Math.sin(Math.PI / 2 * t - Math.PI / 2); },
+  sineOut: function (t) { return Math.sin(Math.PI / 2 * t); },
+  sineInOut: function (t) { return (1 + Math.sin(Math.PI * t - Math.PI / 2)) / 2; },
+  bounce: function (t) {
+    var s = 7.5625;
+    var p = 2.75;
+
+    if (t < 1 / p) {
+      return s * t * t;
+    }
+    if (t < 2 / p) {
+      t -= 1.5 / p;
+      return s * t * t + 0.75;
+    }
+    if (t < 2.5 / p) {
+      t -= 2.25 / p;
+      return s * t * t + 0.9375;
+    }
+    t -= 2.625 / p;
+    return s * t * t + 0.984375;
   }
 };
 
